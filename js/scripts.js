6809dfb878e1f556dfb6384974edb94c8a7e3f17
Calvin Cushman <calvincushman@gmail.com>
2017-01-17 08:52:17 -0800

    remove all front end logic, re-add addition and subtraction funtions

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,22 @@ var multiply = function(number1, number2) {
 };
 
 var divide = function(number1, number2) {
-  return number1 / number2
-}:
+  return number1 / number2;
+};
 
 $(document).ready(function() {
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(promt("Enter another number:"));
-alert(add(number1, number2));
+  $("#addForm").submit(function(event){
+    event.preventDefault();
+    var add1 = parseInt($("#add1").val());
+    var add2 = parseInt($("#add2").val());
+    var result = add(add1, add2);
+    $("#outputAdd").text(result);
+  });
+  $("#subtractForm").submit(function(event){
+    event.preventDefault();
+    var subtract1 = parseInt($("#subtract1").val());
+    var subtract2 = parseInt($("#subtract2").val());
+    var result = subtract(subtract1, subtract2);
+    $("#outputSubtract").text(result);
+  });
 });
